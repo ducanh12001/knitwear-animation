775522936917427b6d5ea6f5e89189fd3695c338
ducanh12001 <ducanh2001.da@gmail.com>
2025-05-19 17:49:24 +0700

    fix: swiper drag and click

--- src/pages/HomePage/SliderSection/ImageSlider.jsx
+++ src/pages/HomePage/SliderSection/ImageSlider.jsx
@@ -1,22 +1,17 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+import { Controller, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Link } from "react-router";
 
-export function ImageSlider({ slides, swiperRef, onSwiper }) {
+export function ImageSlider({ slides, onSwiper }) {
   return (
     <Swiper
-      modules={[Navigation]}
-      navigation={{
-        prevEl: ".swiper-button-prev",
-        nextEl: ".swiper-button-next",
-      }}
+      modules={[Navigation, Controller]}
       spaceBetween={0}
       slidesPerView={1}
       loop={true}
       className="imagesSlider relative !z-10 h-full w-full"
-      ref={swiperRef}
       onSwiper={onSwiper}
       speed={1500}
     >
@@ -27,8 +22,7 @@ export function ImageSlider({ slides, swiperRef, onSwiper }) {
               <img
                 src={slide.desktopImg}
                 alt={slide.title}
-                className="block h-full w-full object-cover object-center"
-                style={{ objectPosition: "center bottom" }}
+                className="block h-full w-full object-cover object-[center_bottom]"
               />
             </div>
             <div className="slide-image relative block h-full w-full md:hidden">
